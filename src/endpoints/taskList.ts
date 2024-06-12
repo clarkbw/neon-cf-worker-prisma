beb783d779b3e0413ec0f9d87f4d2d8b777de223
Bryan Clark <bryan@neon.tech>
2024-06-11 20:36:56 -0700

    white space

--- src/endpoints/taskList.ts
+++ src/endpoints/taskList.ts
@@ -1,66 +1,72 @@
 import {
-	OpenAPIRoute,
-	OpenAPIRouteSchema,
-	Query,
+  OpenAPIRoute,
+  OpenAPIRouteSchema,
+  Query,
 } from "@cloudflare/itty-router-openapi";
 import { Task } from "../types";
+// import { getClient } from "../db";
 
 export class TaskList extends OpenAPIRoute {
-	static schema: OpenAPIRouteSchema = {
-		tags: ["Tasks"],
-		summary: "List Tasks",
-		parameters: {
-			page: Query(Number, {
-				description: "Page number",
-				default: 0,
-			}),
-			isCompleted: Query(Boolean, {
-				description: "Filter by completed flag",
-				required: false,
-			}),
-		},
-		responses: {
-			"200": {
-				description: "Returns a list of tasks",
-				schema: {
-					success: Boolean,
-					result: {
-						tasks: [Task],
-					},
-				},
-			},
-		},
-	};
+  static schema: OpenAPIRouteSchema = {
+    tags: ["Tasks"],
+    summary: "List Tasks",
+    parameters: {
+      page: Query(Number, {
+        description: "Page number",
+        default: 0,
+      }),
+      isCompleted: Query(Boolean, {
+        description: "Filter by completed flag",
+        required: false,
+      }),
+    },
+    responses: {
+      "200": {
+        description: "Returns a list of tasks",
+        schema: {
+          success: Boolean,
+          result: {
+            tasks: [Task],
+          },
+        },
+      },
+    },
+  };
 
-	async handle(
-		request: Request,
-		env: any,
-		context: any,
-		data: Record<string, any>
-	) {
-		// Retrieve the validated parameters
-		const { page, isCompleted } = data.query;
+  async handle(
+    request: Request,
+    env: any,
+    context: any,
+    data: Record<string, any>,
+  ) {
+    // Retrieve the validated parameters
+    const { page, isCompleted } = data.query;
 
-		// Implement your own object list here
+    // Implement your own object list here
+    // const client = getClient();
+    // const { rows } = await client.query("SELECT * FROM tasks WHERE completed = ?;");
+    // const tasks = JSON.stringify(rows);
 
-		return {
-			success: true,
-			tasks: [
-				{
-					name: "Clean my room",
-					slug: "clean-room",
-					description: null,
-					completed: false,
-					due_date: "2025-01-05",
-				},
-				{
-					name: "Build something awesome with Cloudflare Workers",
-					slug: "cloudflare-workers",
-					description: "Lorem Ipsum",
-					completed: true,
-					due_date: "2022-12-24",
-				},
-			],
-		};
-	}
+    const tasks = [
+      {
+        name: "Clean my room",
+        slug: "clean-room",
+        description: null,
+        completed: false,
+        due_date: "2025-01-05",
+      },
+      {
+        name: "Build something awesome with Neon & Cloudflare Workers",
+        slug: "cloudflare-workers",
+        description: "Lorem Ipsum",
+        completed: true,
+        due_date: "2022-12-24",
+      },
+    ].filter((t) => isCompleted === undefined || t.completed == isCompleted);
+
+    return {
+      success: true,
+      tasks,
+    };
+  }
 }
